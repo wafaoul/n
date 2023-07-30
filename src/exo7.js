4e4b376fc0800329f35320960e2417512310315c
wafaoul <134451735+wafaoul@users.noreply.github.com>
2023-07-30 12:13:31 +0200

    Add files via upload

---
+++ src/exo7.js
@@ -0,0 +1,12 @@
+const notes = class notes {
+    constructor(note, moyenne)
+    this.el = document.body;
+this.bord = [];
+this.moyenne = (somme + this.note) / 2;
+
+};
+function computeNotes() {
+    const notes = computeNotes.notes;
+    if moyenne = (somme + this.note) / 2
+    return true;
+};
