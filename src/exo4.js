ddba75334bd0cd89409db4b805bbcbe71fa19fce
wafaoul <134451735+wafaoul@users.noreply.github.com>
2023-07-30 12:11:42 +0200

    Add files via upload

---
+++ src/exo4.js
@@ -0,0 +1,8 @@
+const renderWordMap = class renderWordMap {
+    constructor() {
+        this.el = document.body;
+        this.bord = [['', '', ''], ['', '', ''], ['', '', '']];
+        this.renderGrid = '';
+
+
+        export default renderWordMap;
