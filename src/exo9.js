57361eca759476af20eaa33936aa4fda806696ed
wafaoul <134451735+wafaoul@users.noreply.github.com>
2023-07-30 12:23:02 +0200

    Add files via upload

---
+++ src/exo9.js
@@ -0,0 +1,10 @@
+const Palindrom = class Palindrom {
+    constructor(str) {
+        this.el = document.body;
+        this.str = str;
+    }
+};
+checkPalindrome() {
+    return
+}
+export default Palindrom;
